caf09083bce402cfacc74e8179a548b70df65097
Andrey Chepurnoy <noffily@gmail.com>
2021-10-25 13:23:31 +0300

    Added Depends and Skipped attributes.

---
+++ Attribute/Depends.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Noffily\Teapot\Attribute;
+
+use Attribute;
+
+#[Attribute(Attribute::TARGET_METHOD|Attribute::IS_REPEATABLE)]
+class Depends
+{
+    private string $on;
+
+    public function __construct(string $on)
+    {
+        $this->on = $on;
+    }
+
+    public function getOn(): string
+    {
+        return $this->on;
+    }
+}
---
+++ Attribute/Skipped.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Noffily\Teapot\Attribute;
+
+use Attribute;
+
+#[Attribute(Attribute::TARGET_METHOD)]
+class Skipped
+{
+}
